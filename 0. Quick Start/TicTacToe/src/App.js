09f22fe90d57dd4a65dd137f4f91db8e1efbf8ad
SYUREU <tor01237@gmail.com>
2023-04-18 22:01:50 +0900

    2. Rewrite Board to use two loops to make the squares instead of hardcoding them.

--- 0. Quick Start/TicTacToe/src/App.js
+++ 0. Quick Start/TicTacToe/src/App.js
@@ -30,26 +30,30 @@ function Board({ xIsNext, squares, onPlay }) {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
-  return (
-    <>
-      <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
-    </>
-  );
+  function makeBoard() {
+    let row = [];
+    for (let i = 0; i < 3; ++i) {
+      let column = [];
+      for (let j = 0; j < 3; ++j) {
+        column.push(
+          <Square
+            value={squares[i * 3 + j]}
+            onSquareClick={() => handleClick(i * 3 + j)}
+          />
+        );
+      }
+      row.push(<div className="board-row">{column}</div>);
+    }
+
+    return (
+      <>
+        <div className="status">{status}</div>
+        {row}
+      </>
+    );
+  }
+
+  return makeBoard();
 }
 
 export default function Game() {
