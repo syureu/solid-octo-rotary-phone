d4f50055905d664a3c8c3c57b1aecbb087f45ad4
syureu <tor01237@gmail.com>
2023-04-26 17:09:26 +0900

    Your First Component
    Challenge 1 : Export the component
    Problem

--- 2. Describing the UI/1. Your First Component/src/App.js
+++ 2. Describing the UI/1. Your First Component/src/App.js
@@ -1,5 +1,16 @@
 import React from "react";
 
-export default function Profile() {
-  return <img src="https://i.imgur.com/MK3eW3Am.jpg" alt="Katherine Johnson" />;
+function Profile() {
+  return <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />;
+}
+
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
 }
