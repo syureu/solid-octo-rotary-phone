e1b40d519b18aacd58e8973ed3583d3675564d74
syureu <tor01237@gmail.com>
2023-04-27 23:00:50 +0900

    Importing and Exporting Components
    WIP

--- 2. Describing the UI/2. Importing and Exporting Components/src/App.js
+++ 2. Describing the UI/2. Importing and Exporting Components/src/App.js
@@ -1,19 +1,5 @@
-function Profile() {
-  return (
-    <img
-      src="https://i.imgur.com/MK3eW3As.jpg"
-      alt="Katherine Johnson"
-    />
-  );
-}
+import Gallery from "./Gallery.js";
 
-export default function Gallery() {
-  return (
-    <section>
-      <h1>Amazing scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-    </section>
-  );
+export default function App() {
+  return <Gallery />;
 }
