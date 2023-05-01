57c82f7cf9c16d5c3eabb80b0308a01600aff7aa
syureu <tor01237@gmail.com>
2023-05-01 23:39:27 +0900

    JavaScript in JSX with Curly Braces
    WIP

---
+++ 2. Describing the UI/4. JavaScript in JSX with Culry Braces/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Avatar() {
+  return (
+    <img
+      className="avatar"
+      src="https://i.imgur.com/7vQD0fPs.jpg"
+      alt="Gregorio Y. Zara"
+    />
+  );
+}
