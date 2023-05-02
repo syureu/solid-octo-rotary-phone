0188d37903f10132c725e112b5abd783465ae7b6
syureu <tor01237@gmail.com>
2023-05-02 21:03:13 +0900

    JavaScript in JSX with Curly Braces
    WIP

--- 2. Describing the UI/4. JavaScript in JSX with Culry Braces/src/App.js
+++ 2. Describing the UI/4. JavaScript in JSX with Culry Braces/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 
-export default function Avatar() {
-  const avatar = "https://i.imgur.com/7vQD0fPs.jpg";
-  const description = "Gregorio Y. Zara";
-  return <img className="avatar" src={avatar} alt={description} />;
+export default function TodoList() {
+  const name = "Gregorio Y. Zara";
+  return <h1>{name}'s To Do List</h1>;
 }
