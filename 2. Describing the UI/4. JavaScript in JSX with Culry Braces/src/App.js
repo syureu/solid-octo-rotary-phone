e8f305edff243e0fd7099766a752a891bb3c85d7
syureu <tor01237@gmail.com>
2023-05-02 21:15:01 +0900

    JavaScript in JSX with Curly Braces
    WIP

--- 2. Describing the UI/4. JavaScript in JSX with Culry Braces/src/App.js
+++ 2. Describing the UI/4. JavaScript in JSX with Culry Braces/src/App.js
@@ -1,16 +1,27 @@
 import React from "react";
 
+const person = {
+  name: "Gregorio Y. Zara",
+  theme: {
+    backgroundColor: "black",
+    color: "pink",
+  },
+};
+
 export default function TodoList() {
   return (
-    <ul
-      style={{
-        backgroundColor: "red",
-        color: "white",
-      }}
-    >
-      <li>Improve the videophone</li>
-      <li>Prepare aeronautics lectures</li>
-      <li>Work on the alcohol-fuelled engine</li>
-    </ul>
+    <div style={person.theme}>
+      <h1>{person.name}'s Todos</h1>
+      <img
+        className="avatar"
+        src="https://i.imgur.com/7vQD0fPs.jpg"
+        alt="Gregorio Y. Zara"
+      />
+      <ul>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fuelled engine</li>
+      </ul>
+    </div>
   );
 }
