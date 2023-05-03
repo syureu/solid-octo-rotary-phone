93fd17711c7abee7c7d2c75f66de0c70c629692e
syureu <tor01237@gmail.com>
2023-05-03 21:42:33 +0900

    Passing Props to a Component Challenge 2 of 3: Adjust the image size based on a prop
    problem

--- 2. Describing the UI/5. Passing Props to a Component/src/App.js
+++ 2. Describing the UI/5. Passing Props to a Component/src/App.js
@@ -1,64 +1,26 @@
 import React from "react";
 import { getImageUrl } from "./utils.js";
 
-export default function Gallery() {
+function Avatar({ person, size }) {
   return (
-    <div>
-      <h1>Notable Scientists</h1>
-      <Profile
-        profile={{
-          name: "Maria Skłodowska-Curie",
-          imageUrl: "szV5sdG",
-          imageWidth: 70,
-          imageHeight: 70,
-          profession: "physicist and chemist",
-          awardCount: 4,
-          awardContent:
-            "(Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)",
-          discovered: "polonium (element)",
-        }}
-      />
-      <Profile
-        profile={{
-          name: "Katsuko Saruhashi",
-          imageUrl: "YfeOqp2",
-          imageWidth: 70,
-          imageHeight: 70,
-          profession: "geochemist",
-          awardCount: 2,
-          awardContent: "(Miyake Prize for geochemistry, Tanaka Prize)",
-          discovered: "a method for measuring carbon dioxide in seawater",
-        }}
-      />
-    </div>
+    <img
+      className="avatar"
+      src={getImageUrl(person, "b")}
+      alt={person.name}
+      width={size}
+      height={size}
+    />
   );
 }
 
-function Profile({ profile }) {
+export default function Profile() {
   return (
-    <section className="profile">
-      <h2>{profile.name}</h2>
-      <img
-        className="avatar"
-        src={getImageUrl(profile.imageUrl)}
-        alt={profile.name}
-        width={profile.imageWidth}
-        height={profile.imageHeight}
-      />
-      <ul>
-        <li>
-          <b>Profession: </b>
-          {profile.profession}
-        </li>
-        <li>
-          <b>Awards: {profile.awardCount} </b>
-          {profile.awardContent}
-        </li>
-        <li>
-          <b>Discovered: </b>
-          {profile.discovered}
-        </li>
-      </ul>
-    </section>
+    <Avatar
+      size={40}
+      person={{
+        name: "Gregorio Y. Zara",
+        imageId: "7vQD0fP",
+      }}
+    />
   );
 }
