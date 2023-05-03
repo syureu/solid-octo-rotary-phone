8e2ebb284e11140370e3c7c14944996e4e7082d6
syureu <tor01237@gmail.com>
2023-05-03 21:52:53 +0900

    Passing Props to a Componen Challenge 3 of 3: Passing JSX in a children prop
    solve

--- 2. Describing the UI/5. Passing Props to a Component/src/App.js
+++ 2. Describing the UI/5. Passing Props to a Component/src/App.js
@@ -3,27 +3,47 @@ import React from "react";
 export default function Profile() {
   return (
     <div>
-      <div className="card">
-        <div className="card-content">
-          <h1>Photo</h1>
-          <img
-            className="avatar"
-            src="https://i.imgur.com/OKS67lhm.jpg"
-            alt="Aklilu Lemma"
-            width={70}
-            height={70}
-          />
-        </div>
-      </div>
-      <div className="card">
-        <div className="card-content">
-          <h1>About</h1>
-          <p>
-            Aklilu Lemma was a distinguished Ethiopian scientist who discovered
-            a natural treatment to schistosomiasis.
-          </p>
-        </div>
-      </div>
+      <Card>
+        <Photo />
+      </Card>
+      <Card>
+        <About />
+      </Card>
     </div>
+  );
+}
+
+function Card({ children }) {
+  return (
+    <div className="card">
+      <div className="card-content">{children}</div>
+    </div>
+  );
+}
+
+function Photo() {
+  return (
+    <React.Fragment>
+      <h1>Photo</h1>
+      <img
+        className="avatar"
+        src="https://i.imgur.com/OKS67lhm.jpg"
+        alt="Aklilu Lemma"
+        width={70}
+        height={70}
+      />
+    </React.Fragment>
+  );
+}
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>
+        Aklilu Lemma was a distinguished Ethiopian scientist who discovered a
+        natural treatment to schistosomiasis.
+      </p>
+    </React.Fragment>
   );
 }
