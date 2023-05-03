ab69dc93f5aa46cda4bf7844451a8ea122281188
syureu <tor01237@gmail.com>
2023-05-03 20:55:59 +0900

    Passing Props to a Component
    WIP

--- 2. Describing the UI/5. Passing Props to a Component/src/App.js
+++ 2. Describing the UI/5. Passing Props to a Component/src/App.js
@@ -1,17 +1,42 @@
 import React from "react";
+import { getImageUrl } from "./utils.js";
 
-function Avatar() {
+function Avatar({ person, size }) {
   return (
     <img
       className="avatar"
-      src="https://i.imgur.com/1bX5QH6.jpg"
-      alt="Lin Lanying"
-      width={100}
-      height={100}
+      src={getImageUrl(person)}
+      alt={person.name}
+      width={size}
+      height={size}
     />
   );
 }
 
 export default function Profile() {
-  return <Avatar />;
+  return (
+    <div>
+      <Avatar
+        size={100}
+        person={{
+          name: "Katsuko Saruhashi",
+          imageId: "YfeOqp2",
+        }}
+      />
+      <Avatar
+        size={80}
+        person={{
+          name: "Aklilu Lemma",
+          imageId: "OKS67lh",
+        }}
+      />
+      <Avatar
+        size={50}
+        person={{
+          name: "Lin Lanying",
+          imageId: "1bX5QH6",
+        }}
+      />
+    </div>
+  );
 }
