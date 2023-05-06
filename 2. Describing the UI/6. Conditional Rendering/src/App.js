249ed04e184318a761cc60326eab3a782bd92311
syureu <tor01237@gmail.com>
2023-05-06 23:56:41 +0900

    Conditional Rendering Challenge 2 of 3: Show the item importance with &&
    problem

--- 2. Describing the UI/6. Conditional Rendering/src/App.js
+++ 2. Describing the UI/6. Conditional Rendering/src/App.js
@@ -1,11 +1,7 @@
 import React from "react";
 
-function Item({ name, isPacked }) {
-  return (
-    <li className="item">
-      {name} {!isPacked ? "❌" : "✔"}
-    </li>
-  );
+function Item({ name, importance }) {
+  return <li className="item">{name}</li>;
 }
 
 export default function PackingList() {
@@ -13,9 +9,9 @@ export default function PackingList() {
     <section>
       <h1>Sally Ride's Packing List</h1>
       <ul>
-        <Item isPacked={true} name="Space suit" />
-        <Item isPacked={true} name="Helmet with a golden leaf" />
-        <Item isPacked={false} name="Photo of Tam" />
+        <Item importance={9} name="Space suit" />
+        <Item importance={0} name="Helmet with a golden leaf" />
+        <Item importance={6} name="Photo of Tam" />
       </ul>
     </section>
   );
