c24efc15b8c6f0ccd02688c5a32ab2756412e5ac
syureu <tor01237@gmail.com>
2023-05-08 20:03:28 +0900

    Rendering Lists
    WIP

--- 2. Describing the UI/7. Rendering Lists/src/App.js
+++ 2. Describing the UI/7. Rendering Lists/src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 
-const people = [
-  "Creola Katherine Johnson: mathematician",
-  "Mario José Molina-Pasquel Henríquez: chemist",
-  "Mohammad Abdus Salam: physicist",
-  "Percy Lavon Julian: chemist",
-  "Subrahmanyan Chandrasekhar: astrophysicist",
-];
+import { people } from "./data.js";
+import { getImageUrl } from "./utils.js";
 
 export default function List() {
-  const listItems = people.map((person) => <li>{person}</li>);
+  const chemists = people.filter((person) => person.profession === "chemist");
+  const listItems = chemists.map((person) => (
+    <li>
+      <img src={getImageUrl(person)} alt={person.name} />
+      <p>
+        <b>{person.name}:</b>
+        {" " + person.profession + " "}
+        known for {person.accomplishment}
+      </p>
+    </li>
+  ));
   return <ul>{listItems}</ul>;
 }
