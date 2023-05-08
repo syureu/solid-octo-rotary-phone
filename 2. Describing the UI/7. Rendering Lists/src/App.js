32736c31cc7c155c65468db28c2b751b9e7c6622
syureu <tor01237@gmail.com>
2023-05-08 20:25:00 +0900

    Rendering Lists
    WIP

--- 2. Describing the UI/7. Rendering Lists/src/App.js
+++ 2. Describing the UI/7. Rendering Lists/src/App.js
@@ -4,12 +4,11 @@ import { people } from "./data.js";
 import { getImageUrl } from "./utils.js";
 
 export default function List() {
-  const chemists = people.filter((person) => person.profession === "chemist");
-  const listItems = chemists.map((person) => (
-    <li>
+  const listItems = people.map((person) => (
+    <li key={person.id}>
       <img src={getImageUrl(person)} alt={person.name} />
       <p>
-        <b>{person.name}:</b>
+        <b>{person.name}</b>
         {" " + person.profession + " "}
         known for {person.accomplishment}
       </p>
