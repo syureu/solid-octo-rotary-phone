f23deb1f55c8050f225f0fd1f7f2d53295d4a8cf
syureu <tor01237@gmail.com>
2023-05-09 20:49:27 +0900

    Rendering Lists Challenge 2 of 4: Nested lists in one component
    problem

--- 2. Describing the UI/7. Rendering Lists/src/App.js
+++ 2. Describing the UI/7. Rendering Lists/src/App.js
@@ -1,31 +1,9 @@
-import React from "react";
-import { people } from "./data.js";
-import { getImageUrl } from "./utils.js";
+import { recipes } from "./data.js";
 
-export default function List() {
-  const listMapFunction = (person) => (
-    <li key={person.id}>
-      <img src={getImageUrl(person)} alt={person.name} />
-      <p>
-        <b>{person.name}:</b>
-        {" " + person.profession + " "}
-        known for {person.accomplishment}
-      </p>
-    </li>
-  );
-  const chemists = people
-    .filter((person) => person.profession === "chemist")
-    .map(listMapFunction);
-  const everyoneElse = people
-    .filter((person) => person.profession !== "chemist")
-    .map(listMapFunction);
+export default function RecipeList() {
   return (
-    <article>
-      <h1>Scientists</h1>
-      <h2>Chemists</h2>
-      <ul>{chemists}</ul>
-      <h2>Else</h2>
-      <ul>{everyoneElse}</ul>
-    </article>
+    <div>
+      <h1>Recipes</h1>
+    </div>
   );
 }
