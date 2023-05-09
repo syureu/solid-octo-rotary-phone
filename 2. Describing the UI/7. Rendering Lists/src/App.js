61951bdb0b2b7a90045df2c1f0adeb9e1ae13882
syureu <tor01237@gmail.com>
2023-05-09 20:51:41 +0900

    Rendering Lists Challenge 2 of 4: Nested lists in one component
    solve

--- 2. Describing the UI/7. Rendering Lists/src/App.js
+++ 2. Describing the UI/7. Rendering Lists/src/App.js
@@ -1,9 +1,23 @@
+import React from "react";
 import { recipes } from "./data.js";
 
 export default function RecipeList() {
+  const list = recipes.map((recipe) => {
+    const ingredientsList = recipe.ingredients.map((ingredient) => {
+      return <li>{ingredient}</li>;
+    });
+    return (
+      <li key={recipe.id}>
+        <h2>{recipe.name}</h2>
+        <ul>{ingredientsList}</ul>
+      </li>
+    );
+  });
+
   return (
     <div>
       <h1>Recipes</h1>
+      <ul>{list}</ul>
     </div>
   );
 }
