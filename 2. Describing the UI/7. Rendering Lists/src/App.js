1c6e66a323f8673c7aa94f59ebd0a566e11b0854
syureu <tor01237@gmail.com>
2023-05-09 20:56:20 +0900

    Rendering Lists Challenge 3 of 4: Extracting a list item component
    problem

--- 2. Describing the UI/7. Rendering Lists/src/App.js
+++ 2. Describing the UI/7. Rendering Lists/src/App.js
@@ -1,23 +1,19 @@
-import React from "react";
 import { recipes } from "./data.js";
 
 export default function RecipeList() {
-  const list = recipes.map((recipe) => {
-    const ingredientsList = recipe.ingredients.map((ingredient) => {
-      return <li>{ingredient}</li>;
-    });
-    return (
-      <li key={recipe.id}>
-        <h2>{recipe.name}</h2>
-        <ul>{ingredientsList}</ul>
-      </li>
-    );
-  });
-
   return (
     <div>
       <h1>Recipes</h1>
-      <ul>{list}</ul>
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h2>{recipe.name}</h2>
+          <ul>
+            {recipe.ingredients.map((ingredient) => (
+              <li key={ingredient}>{ingredient}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
