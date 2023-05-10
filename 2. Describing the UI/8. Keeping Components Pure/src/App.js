07bb5dd7a5df9d9e184a1c79ca2f8383f685c323
syureu <tor01237@gmail.com>
2023-05-10 21:26:13 +0900

    Keeping Components Pure
    WIP

--- 2. Describing the UI/8. Keeping Components Pure/src/App.js
+++ 2. Describing the UI/8. Keeping Components Pure/src/App.js
@@ -1,25 +1,19 @@
 import React from "react";
 
-function Recipe({ drinkers }) {
-  return (
-    <ol>
-      <li>Boil {drinkers} cups of water.</li>
-      <li>
-        Add {drinkers} spoons of tea and {0.5 * drinkers} spoons of spice.
-      </li>
-      <li>Add {0.5 * drinkers} cups of milk to boil and sugar to taste.</li>
-    </ol>
-  );
+let guest = 0;
+
+function Cup() {
+  // Bad: changing a preexisting variable!
+  guest = guest + 1;
+  return <h2>Tea cup for guest #{guest}</h2>;
 }
 
-export default function App() {
+export default function TeaSet() {
   return (
-    <section>
-      <h1>Spiced Chai Recipe</h1>
-      <h2>For two</h2>
-      <Recipe drinkers={2} />
-      <h2>For a gathering</h2>
-      <Recipe drinkers={4} />
-    </section>
+    <>
+      <Cup />
+      <Cup />
+      <Cup />
+    </>
   );
 }
