116e176c2a759e3e370f3e7cf5963003a8e1563f
syureu <tor01237@gmail.com>
2023-05-11 20:58:02 +0900

    Keeping Components Pure Challenge 3 of 3: Fix a broken story tray
    problem

--- 2. Describing the UI/8. Keeping Components Pure/src/App.js
+++ 2. Describing the UI/8. Keeping Components Pure/src/App.js
@@ -1,22 +1,44 @@
 import React from "react";
 
-import Profile from "./Profile.js";
+import { useState, useEffect } from "react";
+import StoryTray from "./StoryTray.js";
+
+let initialStories = [
+  { id: 0, label: "Ankit's Story" },
+  { id: 1, label: "Taylor's Story" },
+];
 
 export default function App() {
+  let [stories, setStories] = useState([...initialStories]);
+  let time = useTime();
+
+  // HACK: Prevent the memory from growing forever while you read docs.
+  // We're breaking our own rules here.
+  if (stories.length > 100) {
+    stories.length = 100;
+  }
+
   return (
-    <React.Fragment>
-      <Profile
-        person={{
-          imageId: "lrWQx8l",
-          name: "Subrahmanyan Chandrasekhar",
-        }}
-      />
-      <Profile
-        person={{
-          imageId: "MK3eW3A",
-          name: "Creola Katherine Johnson",
-        }}
-      />
-    </React.Fragment>
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        textAlign: "center",
+      }}
+    >
+      <h2>It is {time.toLocaleTimeString()} now.</h2>
+      <StoryTray stories={stories} />
+    </div>
   );
+}
+
+function useTime() {
+  const [time, setTime] = useState(() => new Date());
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+  return time;
 }
