0e6197b1283d124b99f2f2bb6ea1e004ed53e4fa
syureu <tor01237@gmail.com>
2023-05-11 20:47:51 +0900

    Keeping Components Pure Challenge 2 of 3: Fix a broken profile
    solve

--- 2. Describing the UI/8. Keeping Components Pure/src/Profile.js
+++ 2. Describing the UI/8. Keeping Components Pure/src/Profile.js
@@ -3,28 +3,25 @@ import React from "react";
 import Panel from "./Panel.js";
 import { getImageUrl } from "./utils.js";
 
-let currentPerson;
-
 export default function Profile({ person }) {
-  currentPerson = person;
   return (
     <Panel>
-      <Header />
-      <Avatar />
+      <Header name={person.name} />
+      <Avatar person={person} />
     </Panel>
   );
 }
 
-function Header() {
-  return <h1>{currentPerson.name}</h1>;
+function Header({ name }) {
+  return <h1>{name}</h1>;
 }
 
-function Avatar() {
+function Avatar({ person }) {
   return (
     <img
       className="avatar"
-      src={getImageUrl(currentPerson)}
-      alt={currentPerson.name}
+      src={getImageUrl(person)}
+      alt={person.name}
       width={50}
       height={50}
     />
