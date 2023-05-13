e7e314ab86c12bbce40ef21ac2eff653d0004181
syureu <tor01237@gmail.com>
2023-05-13 19:47:17 +0900

    Responding to Events
    WIP

--- 3. Adding Interactivity/1. Responding to Events/src/App.js
+++ 3. Adding Interactivity/1. Responding to Events/src/App.js
@@ -1,9 +1,26 @@
 import React from "react";
 
-export default function Button() {
-  function handleClick() {
-    alert("You clicked me!");
+function Button({ onClick, children }) {
+  return <button onClick={onClick}>{children}</button>;
+}
+
+function PlayButton({ movieName }) {
+  function handlePlayClick() {
+    alert(`Playing ${movieName}!`);
   }
 
-  return <button onClick={handleClick}>Click me</button>;
+  return <Button onClick={handlePlayClick}>Play "{movieName}"</Button>;
+}
+
+function UploadButton() {
+  return <Button onClick={() => alert("Uploading!")}>Upload Image</Button>;
+}
+
+export default function Toolbar() {
+  return (
+    <div>
+      <PlayButton movieName="Kiki's Delivery Service" />
+      <UploadButton />
+    </div>
+  );
 }
