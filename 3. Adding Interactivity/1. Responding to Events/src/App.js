ca9f79c0f86d946ee2531a3430f53f5d9432d09d
syureu <tor01237@gmail.com>
2023-05-14 16:41:29 +0900

    Responding to Events
    WIP

--- 3. Adding Interactivity/1. Responding to Events/src/App.js
+++ 3. Adding Interactivity/1. Responding to Events/src/App.js
@@ -1,28 +1,10 @@
 import React from "react";
 
-function Button({ onClick, children }) {
+export default function Signup() {
   return (
-    <button
-      onClick={(e) => {
-        e.stopPropagation();
-        onClick();
-      }}
-    >
-      {children}
-    </button>
-  );
-}
-
-export default function Toolbar() {
-  return (
-    <div
-      className="Toolbar"
-      onClick={() => {
-        alert("You clicked on the toolbar!");
-      }}
-    >
-      <Button onClick={() => alert("Playing!")}>Play Movie</Button>
-      <Button onClick={() => alert("Uploading!")}>Upload Image</Button>
-    </div>
+    <form onSubmit={() => alert("Submitting!")}>
+      <input />
+      <button>Send</button>
+    </form>
   );
 }
