27be8dcb9f83d4496f1d1e25fafa8080efcdf6a7
syureu <tor01237@gmail.com>
2023-05-14 17:18:51 +0900

    Responding to Events Challenge 1 of 2: Fix an event handler
    problem

--- 3. Adding Interactivity/1. Responding to Events/src/App.js
+++ 3. Adding Interactivity/1. Responding to Events/src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 
-export default function Signup() {
-  return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        alert("Submitting!");
-      }}
-    >
-      <input />
-      <button>Send</button>
-    </form>
-  );
+export default function LightSwitch() {
+  function handleClick() {
+    let bodyStyle = document.body.style;
+    if (bodyStyle.backgroundColor === "black") {
+      bodyStyle.backgroundColor = "white";
+    } else {
+      bodyStyle.backgroundColor = "black";
+    }
+  }
+
+  return <button onClick={handleClick()}>Toggle the lights</button>;
 }
