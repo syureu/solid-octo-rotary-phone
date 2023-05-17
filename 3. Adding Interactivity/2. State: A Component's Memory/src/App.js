a3148d6edc1ef327e7665f21c8059a73df88f1ae
syureu <tor01237@gmail.com>
2023-05-17 19:41:53 +0900

    State: A Component's Memory
    WIP

--- 3. Adding Interactivity/2. State: A Component's Memory/src/App.js
+++ 3. Adding Interactivity/2. State: A Component's Memory/src/App.js
@@ -5,15 +5,20 @@ import { sculptureList } from "./data.js";
 
 export default function Gallery() {
   const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
 
-  function handleClick() {
+  function handleNextClick() {
     setIndex(index + 1);
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
   }
 
   let sculpture = sculptureList[index];
   return (
     <React.Fragment>
-      <button onClick={handleClick}>Next</button>
+      <button onClick={handleNextClick}>Next</button>
       <h2>
         <i>{sculpture.name} </i>
         by {sculpture.artist}
@@ -21,8 +26,11 @@ export default function Gallery() {
       <h3>
         ({index + 1} of {sculptureList.length})
       </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? "Hide" : "Show"} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
       <img src={sculpture.url} alt={sculpture.alt} />
-      <p>{sculpture.description}</p>
     </React.Fragment>
   );
 }
