37478d3ce80dfe12499409ec4f4a61a6919eda65
syureu <tor01237@gmail.com>
2023-05-18 19:56:24 +0900

    State: A Component's Memory Challenge 4 of 4: Remove unnecessary state
    problem

--- 3. Adding Interactivity/2. State: A Component's Memory/src/App.js
+++ 3. Adding Interactivity/2. State: A Component's Memory/src/App.js
@@ -2,28 +2,12 @@ import React from "react";
 import { useState } from "react";
 
 export default function FeedbackForm() {
-  const [isSent, setIsSent] = useState(false);
-  const [message, setMessage] = useState("");
-  if (isSent) {
-    return <h1>Thank you!</h1>;
-  } else {
-    // eslint-disable-next-line
-    return (
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          alert(`Sending: "${message}"`);
-          setIsSent(true);
-        }}
-      >
-        <textarea
-          placeholder="Message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-        <br />
-        <button type="submit">Send</button>
-      </form>
-    );
+  const [name, setName] = useState("");
+
+  function handleClick() {
+    setName(prompt("What is your name?"));
+    alert(`Hello, ${name}!`);
   }
+
+  return <button onClick={handleClick}>Greet</button>;
 }
