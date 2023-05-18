66087780daa809155d69a8e90d7c5882c9e19547
syureu <tor01237@gmail.com>
2023-05-18 19:56:42 +0900

    State: A Component's Memory Challenge 4 of 4: Remove unnecessary state
    solve

--- 3. Adding Interactivity/2. State: A Component's Memory/src/App.js
+++ 3. Adding Interactivity/2. State: A Component's Memory/src/App.js
@@ -1,11 +1,8 @@
 import React from "react";
-import { useState } from "react";
 
 export default function FeedbackForm() {
-  const [name, setName] = useState("");
-
   function handleClick() {
-    setName(prompt("What is your name?"));
+    let name = prompt("What is your name?");
     alert(`Hello, ${name}!`);
   }
 
