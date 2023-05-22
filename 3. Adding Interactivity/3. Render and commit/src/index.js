311582764b6450f90bdb2120815656ea837a04c4
syureu <tor01237@gmail.com>
2023-05-22 21:10:43 +0900

    Render and commit
    WIP

---
+++ 3. Adding Interactivity/3. Render and commit/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import Image from "./Image.js";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<Image />);
