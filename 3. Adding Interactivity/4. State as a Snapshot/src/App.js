c7b6b88fb0a1443d5e275058f4caaad31f2f0d04
syureu <tor01237@gmail.com>
2023-05-23 22:14:11 +0900

    State as a snapshot Challenge 1 of 1: Implement a traffic light
    solve

--- 3. Adding Interactivity/4. State as a Snapshot/src/App.js
+++ 3. Adding Interactivity/4. State as a Snapshot/src/App.js
@@ -4,19 +4,28 @@ import { useState } from "react";
 export default function TrafficLight() {
   const [walk, setWalk] = useState(true);
 
+  function getwalkStateString(toggleReturn) {
+    if (toggleReturn) {
+      return walk ? "Walk" : "Stop";
+    } else {
+      return walk ? "Stop" : "Walk";
+    }
+  }
+
   function handleClick() {
     setWalk(!walk);
+    alert(getwalkStateString() + " is next");
   }
 
   return (
     <React.Fragment>
-      <button onClick={handleClick}>Change to {walk ? "Stop" : "Walk"}</button>
+      <button onClick={handleClick}>Change to {getwalkStateString()}</button>
       <h1
         style={{
           color: walk ? "darkgreen" : "darkred",
         }}
       >
-        {walk ? "Walk" : "Stop"}
+        {getwalkStateString(true)}
       </h1>
     </React.Fragment>
   );
