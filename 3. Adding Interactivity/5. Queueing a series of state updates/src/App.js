8b2bae0ab5a3147d7b7da91bb07d2bfe30f99e06
syureu <tor01237@gmail.com>
2023-06-01 22:40:16 +0900

    Queueing a series of state updates
    WIP

--- 3. Adding Interactivity/5. Queueing a series of state updates/src/App.js
+++ 3. Adding Interactivity/5. Queueing a series of state updates/src/App.js
@@ -8,9 +8,9 @@ export default function Counter() {
       <h1>{number}</h1>
       <button
         onClick={() => {
-          setNumber(number + 1);
-          setNumber(number + 1);
-          setNumber(number + 1);
+          setNumber((n) => n + 1);
+          setNumber((n) => n + 1);
+          setNumber((n) => n + 1);
         }}
       >
         +3
