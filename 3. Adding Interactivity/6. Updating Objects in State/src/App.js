2eb34478f554bcf023b3cacfc2fa90a7540ace42
syureu <tor01237@gmail.com>
2023-06-04 22:08:08 +0900

    Updating Objects in State
    WIP

--- 3. Adding Interactivity/6. Updating Objects in State/src/App.js
+++ 3. Adding Interactivity/6. Updating Objects in State/src/App.js
@@ -9,8 +9,10 @@ export default function MovingDot() {
   return (
     <div
       onPointerMove={(e) => {
-        position.x = e.clientX;
-        position.y = e.clientY;
+        setPosition({
+          x: e.clientX,
+          y: e.clientY,
+        });
       }}
       style={{
         position: "relative",
