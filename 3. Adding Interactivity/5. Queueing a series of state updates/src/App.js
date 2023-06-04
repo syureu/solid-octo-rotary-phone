16c6df07d03cafa2684be87df73ebff16da271d6
syureu <tor01237@gmail.com>
2023-06-04 16:05:03 +0900

    Queueing a Series of State Updates Challenge 1 of 2: Fix a request counter
    problem

--- 3. Adding Interactivity/5. Queueing a series of state updates/src/App.js
+++ 3. Adding Interactivity/5. Queueing a series of state updates/src/App.js
@@ -1,20 +1,27 @@
 import React, { useState } from "react";
 
-export default function Counter() {
-  const [number, setNumber] = useState(0);
+export default function RequestTracker() {
+  const [pending, setPending] = useState(0);
+  const [completed, setCompleted] = useState(0);
+
+  async function handleClick() {
+    setPending(pending + 1);
+    await delay(3000);
+    setPending(pending - 1);
+    setCompleted(completed + 1);
+  }
 
   return (
-    <React.Fragment>
-      <h1>{number}</h1>
-      <button
-        onClick={() => {
-          setNumber(number + 5);
-          setNumber((n) => n + 1);
-          setNumber(42);
-        }}
-      >
-        Increase the number
-      </button>
-    </React.Fragment>
+    <>
+      <h3>Pending: {pending}</h3>
+      <h3>Completed: {completed}</h3>
+      <button onClick={handleClick}>Buy</button>
+    </>
   );
+}
+
+function delay(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
