b342d8fadc784f6923c2ecd9a023a120a841a2d1
syureu <tor01237@gmail.com>
2023-06-08 20:45:42 +0900

    Updating Objects in State Challenge 2 of 3: Find and fix the mutation
    problem

--- 3. Adding Interactivity/6. Updating Objects in State/src/App.js
+++ 3. Adding Interactivity/6. Updating Objects in State/src/App.js
@@ -1,49 +1,43 @@
 import React from "react";
 
 import { useState } from "react";
+import Background from "./Background.js";
+import Box from "./Box.js";
 
-export default function Scoreboard() {
-  const [player, setPlayer] = useState({
-    firstName: "Ranjani",
-    lastName: "Shettar",
-    score: 10,
+const initialPosition = {
+  x: 0,
+  y: 0,
+};
+
+export default function Canvas() {
+  const [shape, setShape] = useState({
+    color: "orange",
+    position: initialPosition,
   });
 
-  function handlePlusClick() {
-    setPlayer({
-      ...player,
-      score: player.score + 1,
-    });
+  function handleMove(dx, dy) {
+    shape.position.x += dx;
+    shape.position.y += dy;
   }
 
-  function handleFirstNameChange(e) {
-    setPlayer({
-      ...player,
-      firstName: e.target.value,
-    });
-  }
-
-  function handleLastNameChange(e) {
-    setPlayer({
-      ...player,
-      lastName: e.target.value,
+  function handleColorChange(e) {
+    setShape({
+      ...shape,
+      color: e.target.value,
     });
   }
 
   return (
-    <React.Fragment>
-      <label>
-        Score: <b>{player.score}</b>{" "}
-        <button onClick={handlePlusClick}>+1</button>
-      </label>
-      <label>
-        First name:
-        <input value={player.firstName} onChange={handleFirstNameChange} />
-      </label>
-      <label>
-        Last name:
-        <input value={player.lastName} onChange={handleLastNameChange} />
-      </label>
-    </React.Fragment>
+    <>
+      <select value={shape.color} onChange={handleColorChange}>
+        <option value="orange">orange</option>
+        <option value="lightpink">lightpink</option>
+        <option value="aliceblue">aliceblue</option>
+      </select>
+      <Background position={initialPosition} />
+      <Box color={shape.color} position={shape.position} onMove={handleMove}>
+        Drag me!
+      </Box>
+    </>
   );
 }
---
+++ 3. Adding Interactivity/6. Updating Objects in State/src/Background.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Background({ position }) {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        transform: `translate(
+          ${position.x}px,
+          ${position.y}px
+        )`,
+        width: 250,
+        height: 250,
+        backgroundColor: "rgba(200, 200, 0, 0.2)",
+      }}
+    />
+  );
+}
