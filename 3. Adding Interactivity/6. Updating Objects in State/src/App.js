a2b29c78098d7a2a82a6841c61f51b16ae7cad19
syureu <tor01237@gmail.com>
2023-06-08 20:17:32 +0900

    Updating Objects in State
    WIP

--- 3. Adding Interactivity/6. Updating Objects in State/src/App.js
+++ 3. Adding Interactivity/6. Updating Objects in State/src/App.js
@@ -4,49 +4,77 @@ import { useState } from "react";
 
 export default function Form() {
   const [person, setPerson] = useState({
-    firstName: "Barbara",
-    lastName: "Hepworth",
-    email: "bhepworth@sculpture.com",
+    name: "Niki de Saint Phalle",
+    artwork: {
+      title: "Blue Nana",
+      city: "Hamburg",
+      image: "https://i.imgur.com/Sd1AgUOm.jpg",
+    },
   });
 
-  function handleFirstNameChange(e) {
+  function handleNameChange(e) {
     setPerson({
       ...person,
-      firstName: e.target.value,
+      name: e.target.value,
     });
   }
 
-  function handleLastNameChange(e) {
+  function handleTitleChange(e) {
     setPerson({
       ...person,
-      lastName: e.target.value,
+      artwork: {
+        ...person.artwork,
+        title: e.target.value,
+      },
     });
   }
 
-  function handleEmailChange(e) {
+  function handleCityChange(e) {
     setPerson({
       ...person,
-      email: e.target.value,
+      artwork: {
+        ...person.artwork,
+        city: e.target.value,
+      },
+    });
+  }
+
+  function handleImageChange(e) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        image: e.target.value,
+      },
     });
   }
 
   return (
     <React.Fragment>
       <label>
-        First name:
-        <input value={person.firstName} onChange={handleFirstNameChange} />
+        Name:
+        <input value={person.name} onChange={handleNameChange} />
       </label>
       <label>
-        Last name:
-        <input value={person.lastName} onChange={handleLastNameChange} />
+        Title:
+        <input value={person.artwork.title} onChange={handleTitleChange} />
       </label>
       <label>
-        Email:
-        <input value={person.email} onChange={handleEmailChange} />
+        City:
+        <input value={person.artwork.city} onChange={handleCityChange} />
+      </label>
+      <label>
+        Image:
+        <input value={person.artwork.image} onChange={handleImageChange} />
       </label>
       <p>
-        {person.firstName} {person.lastName} ({person.email})
+        <i>{person.artwork.title}</i>
+        {" by "}
+        {person.name}
+        <br />
+        (located in {person.artwork.city})
       </p>
+      <img src={person.artwork.image} alt={person.artwork.title} />
     </React.Fragment>
   );
 }
