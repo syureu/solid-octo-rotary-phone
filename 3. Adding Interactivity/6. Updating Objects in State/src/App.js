116929beab1b20565f674b35baa82cc7a736cfbb
syureu <tor01237@gmail.com>
2023-06-08 20:34:01 +0900

    Updating Objects in State Challenge 1 of 3: Fix incorrect state updates
    problem

--- 3. Adding Interactivity/6. Updating Objects in State/src/App.js
+++ 3. Adding Interactivity/6. Updating Objects in State/src/App.js
@@ -1,67 +1,45 @@
 import React from "react";
 
-import { useImmer } from "use-immer";
+import { useState } from "react";
 
-export default function Form() {
-  const [person, updatePerson] = useImmer({
-    name: "Niki de Saint Phalle",
-    artwork: {
-      title: "Blue Nana",
-      city: "Hamburg",
-      image: "https://i.imgur.com/Sd1AgUOm.jpg",
-    },
+export default function Scoreboard() {
+  const [player, setPlayer] = useState({
+    firstName: "Ranjani",
+    lastName: "Shettar",
+    score: 10,
   });
 
-  function handleNameChange(e) {
-    updatePerson((draft) => {
-      draft.name = e.target.value;
+  function handlePlusClick() {
+    player.score++;
+  }
+
+  function handleFirstNameChange(e) {
+    setPlayer({
+      ...player,
+      firstName: e.target.value,
     });
   }
 
-  function handleTitleChange(e) {
-    updatePerson((draft) => {
-      draft.artwork.title = e.target.value;
-    });
-  }
-
-  function handleCityChange(e) {
-    updatePerson((draft) => {
-      draft.artwork.city = e.target.value;
-    });
-  }
-
-  function handleImageChange(e) {
-    updatePerson((draft) => {
-      draft.artwork.image = e.target.value;
+  function handleLastNameChange(e) {
+    setPlayer({
+      lastName: e.target.value,
     });
   }
 
   return (
     <React.Fragment>
       <label>
-        Name:
-        <input value={person.name} onChange={handleNameChange} />
+        Score: <b>{player.score}</b>{" "}
+        <button onClick={handlePlusClick}>+1</button>
       </label>
       <label>
-        Title:
-        <input value={person.artwork.title} onChange={handleTitleChange} />
+        First name:
+        <input value={player.firstName} onChange={handleFirstNameChange} />
       </label>
       <label>
-        City:
-        <input value={person.artwork.city} onChange={handleCityChange} />
+        Last name:
+        <input value={player.lastName} onChange={handleLastNameChange} />
       </label>
-      <label>
-        Image:
-        <input value={person.artwork.image} onChange={handleImageChange} />
-      </label>
-      <p>
-        <i>{person.artwork.title}</i>
-        {" by "}
-        {person.name}
-        <br />
-        (located in {person.artwork.city})
-      </p>
-      <img src={person.artwork.image} alt={person.artwork.title} />
     </React.Fragment>
   );
 }
