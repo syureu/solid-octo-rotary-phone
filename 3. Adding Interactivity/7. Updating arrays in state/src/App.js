791619efa510d0a9cb2614487e598d5ecffcb979
syureu <tor01237@gmail.com>
2023-06-11 23:54:53 +0900

    Updating arrays in state
    WIP

---
+++ 3. Adding Interactivity/7. Updating arrays in state/src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+let nextId = 0;
+
+export default function List() {
+  const [name, setName] = useState("");
+  const [artists, setArtists] = useState([]);
+
+  return (
+    <React.Fragment>
+      <h1>Inspiring sculptors:</h1>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button
+        onClick={() => {
+          artists.push({
+            id: nextId++,
+            name: name,
+          });
+        }}
+      >
+        Add
+      </button>
+      <ul>
+        {artists.map((artist) => (
+          <li key={artist.id}>{artist.name}</li>
+        ))}
+      </ul>
+    </React.Fragment>
+  );
+}
