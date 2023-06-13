b9b634bdffd30b1c13c2657c9767895c24d4491d
syureu <tor01237@gmail.com>
2023-06-13 23:23:08 +0900

    Updating arrays in state
    WIP

--- 3. Adding Interactivity/7. Updating arrays in state/src/App.js
+++ 3. Adding Interactivity/7. Updating arrays in state/src/App.js
@@ -2,49 +2,38 @@ import React from "react";
 
 import { useState } from "react";
 
-let initialShapes = [
-  { id: 0, type: "circle", x: 50, y: 100 },
-  { id: 1, type: "square", x: 150, y: 100 },
-  { id: 2, type: "circle", x: 250, y: 100 },
-];
+let initialCounters = [0, 0, 0];
 
-export default function ShapeEditor() {
-  const [shapes, setShapes] = useState(initialShapes);
+export default function CounterList() {
+  const [counters, setCounters] = useState(initialCounters);
 
-  function handleClick() {
-    const nextShapes = shapes.map((shape) => {
-      if (shape.type === "square") {
-        // No change
-        return shape;
+  function handleIncrementClick(index) {
+    const nextCounters = counters.map((c, i) => {
+      if (i === index) {
+        // Increment the clicked counter
+        return c + 1;
       } else {
-        // Return a new circle 50px below
-        return {
-          ...shape,
-          y: shape.y + 50,
-        };
+        // The rest haven't changed
+        return c;
       }
     });
-    // Re-render with the new array
-    setShapes(nextShapes);
+    setCounters(nextCounters);
   }
 
   return (
-    <React.Fragment>
-      <button onClick={handleClick}>Move circles down!</button>
-      {shapes.map((shape) => (
-        <div
-          key={shape.id}
-          style={{
-            background: "purple",
-            position: "absolute",
-            left: shape.x,
-            top: shape.y,
-            borderRadius: shape.type === "circle" ? "50%" : "",
-            width: 20,
-            height: 20,
-          }}
-        />
+    <ul>
+      {counters.map((counter, i) => (
+        <li key={i}>
+          {counter}
+          <button
+            onClick={() => {
+              handleIncrementClick(i);
+            }}
+          >
+            +1
+          </button>
+        </li>
       ))}
-    </React.Fragment>
+    </ul>
   );
 }
