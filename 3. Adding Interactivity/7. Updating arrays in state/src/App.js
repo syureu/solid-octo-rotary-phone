9c17fbf6bfdf70c1648f9adabe586d4b211dfb9a
syureu <tor01237@gmail.com>
2023-06-13 23:20:57 +0900

    Updating arrays in state
    WIP

--- 3. Adding Interactivity/7. Updating arrays in state/src/App.js
+++ 3. Adding Interactivity/7. Updating arrays in state/src/App.js
@@ -2,32 +2,49 @@ import React from "react";
 
 import { useState } from "react";
 
-let initialArtists = [
-  { id: 0, name: "Marta Colvin Andrade" },
-  { id: 1, name: "Lamidi Olonade Fakeye" },
-  { id: 2, name: "Louise Nevelson" },
+let initialShapes = [
+  { id: 0, type: "circle", x: 50, y: 100 },
+  { id: 1, type: "square", x: 150, y: 100 },
+  { id: 2, type: "circle", x: 250, y: 100 },
 ];
 
-export default function List() {
-  const [artists, setArtists] = useState(initialArtists);
+export default function ShapeEditor() {
+  const [shapes, setShapes] = useState(initialShapes);
+
+  function handleClick() {
+    const nextShapes = shapes.map((shape) => {
+      if (shape.type === "square") {
+        // No change
+        return shape;
+      } else {
+        // Return a new circle 50px below
+        return {
+          ...shape,
+          y: shape.y + 50,
+        };
+      }
+    });
+    // Re-render with the new array
+    setShapes(nextShapes);
+  }
 
   return (
     <React.Fragment>
-      <h1>Inspiring sculptors:</h1>
-      <ul>
-        {artists.map((artist) => (
-          <li key={artist.id}>
-            {artist.name}{" "}
-            <button
-              onClick={() => {
-                setArtists(artists.filter((a) => a.id !== artist.id));
-              }}
-            >
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
+      <button onClick={handleClick}>Move circles down!</button>
+      {shapes.map((shape) => (
+        <div
+          key={shape.id}
+          style={{
+            background: "purple",
+            position: "absolute",
+            left: shape.x,
+            top: shape.y,
+            borderRadius: shape.type === "circle" ? "50%" : "",
+            width: 20,
+            height: 20,
+          }}
+        />
+      ))}
     </React.Fragment>
   );
 }
