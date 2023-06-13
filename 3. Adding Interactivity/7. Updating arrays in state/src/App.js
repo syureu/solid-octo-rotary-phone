f581b76147da396df119ea41b9a1fd9f8be54ad1
syureu <tor01237@gmail.com>
2023-06-13 23:52:43 +0900

    Updating arrays in state
    WIP

--- 3. Adding Interactivity/7. Updating arrays in state/src/App.js
+++ 3. Adding Interactivity/7. Updating arrays in state/src/App.js
@@ -14,17 +14,31 @@ export default function BucketList() {
   const [yourList, setYourList] = useState(initialList);
 
   function handleToggleMyList(artworkId, nextSeen) {
-    const myNextList = [...myList];
-    const artwork = myNextList.find((a) => a.id === artworkId);
-    artwork.seen = nextSeen;
-    setMyList(myNextList);
+    setMyList(
+      myList.map((artwork) => {
+        if (artwork.id === artworkId) {
+          // Create a *new* object with changes
+          return { ...artwork, seen: nextSeen };
+        } else {
+          // No changes
+          return artwork;
+        }
+      })
+    );
   }
 
   function handleToggleYourList(artworkId, nextSeen) {
-    const yourNextList = [...yourList];
-    const artwork = yourNextList.find((a) => a.id === artworkId);
-    artwork.seen = nextSeen;
-    setYourList(yourNextList);
+    setYourList(
+      yourList.map((artwork) => {
+        if (artwork.id === artworkId) {
+          // Create a *new* object with changes
+          return { ...artwork, seen: nextSeen };
+        } else {
+          // No changes
+          return artwork;
+        }
+      })
+    );
   }
 
   return (
