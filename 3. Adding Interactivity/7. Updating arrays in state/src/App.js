0763cd00f2b8c40b65e9c718859f1c7151bde4fe
syureu <tor01237@gmail.com>
2023-06-13 23:33:57 +0900

    Updating arrays in state
    WIP

--- 3. Adding Interactivity/7. Updating arrays in state/src/App.js
+++ 3. Adding Interactivity/7. Updating arrays in state/src/App.js
@@ -3,38 +3,27 @@ import React from "react";
 import { useState } from "react";
 
 let nextId = 3;
-const initialArtists = [
-  { id: 0, name: "Marta Colvin Andrade" },
-  { id: 1, name: "Lamidi Olonade Fakeye" },
-  { id: 2, name: "Louise Nevelson" },
+const initialList = [
+  { id: 0, title: "Big Bellies" },
+  { id: 1, title: "Lunar Landscape" },
+  { id: 2, title: "Terracotta Army" },
 ];
 
 export default function List() {
-  const [name, setName] = useState("");
-  const [artists, setArtists] = useState(initialArtists);
+  const [list, setList] = useState(initialList);
 
   function handleClick() {
-    const insertAt = 1; // Could be any index
-    const nextArtists = [
-      // Items before the insertion point:
-      ...artists.slice(0, insertAt),
-      // New item:
-      { id: nextId++, name: name },
-      // Items after the insertion point:
-      ...artists.slice(insertAt),
-    ];
-    setArtists(nextArtists);
-    setName("");
+    const nextList = [...list];
+    nextList.reverse();
+    setList(nextList);
   }
 
   return (
     <React.Fragment>
-      <h1>Inspiring sculptors:</h1>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
-      <button onClick={handleClick}>Insert</button>
+      <button onClick={handleClick}>Reverse</button>
       <ul>
-        {artists.map((artist) => (
-          <li key={artist.id}>{artist.name}</li>
+        {list.map((artwork) => (
+          <li key={artwork.id}>{artwork.title}</li>
         ))}
       </ul>
     </React.Fragment>
