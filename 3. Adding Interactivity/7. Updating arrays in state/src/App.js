0ceb95c1d696dffa7e82054e17d7e37541a103c0
syureu <tor01237@gmail.com>
2023-06-13 23:25:28 +0900

    Updating arrays in state
    WIP

--- 3. Adding Interactivity/7. Updating arrays in state/src/App.js
+++ 3. Adding Interactivity/7. Updating arrays in state/src/App.js
@@ -2,38 +2,41 @@ import React from "react";
 
 import { useState } from "react";
 
-let initialCounters = [0, 0, 0];
+let nextId = 3;
+const initialArtists = [
+  { id: 0, name: "Marta Colvin Andrade" },
+  { id: 1, name: "Lamidi Olonade Fakeye" },
+  { id: 2, name: "Louise Nevelson" },
+];
 
-export default function CounterList() {
-  const [counters, setCounters] = useState(initialCounters);
+export default function List() {
+  const [name, setName] = useState("");
+  const [artists, setArtists] = useState(initialArtists);
 
-  function handleIncrementClick(index) {
-    const nextCounters = counters.map((c, i) => {
-      if (i === index) {
-        // Increment the clicked counter
-        return c + 1;
-      } else {
-        // The rest haven't changed
-        return c;
-      }
-    });
-    setCounters(nextCounters);
+  function handleClick() {
+    const insertAt = 1; // Could be any index
+    const nextArtists = [
+      // Items before the insertion point:
+      ...artists.slice(0, insertAt),
+      // New item:
+      { id: nextId++, name: name },
+      // Items after the insertion point:
+      ...artists.slice(insertAt),
+    ];
+    setArtists(nextArtists);
+    setName("");
   }
 
   return (
-    <ul>
-      {counters.map((counter, i) => (
-        <li key={i}>
-          {counter}
-          <button
-            onClick={() => {
-              handleIncrementClick(i);
-            }}
-          >
-            +1
-          </button>
-        </li>
-      ))}
-    </ul>
+    <React.Fragment>
+      <h1>Inspiring sculptors:</h1>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={handleClick}>Insert</button>
+      <ul>
+        {artists.map((artist) => (
+          <li key={artist.id}>{artist.name}</li>
+        ))}
+      </ul>
+    </React.Fragment>
   );
 }
