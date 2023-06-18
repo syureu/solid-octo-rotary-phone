70965d1bbb68ddd44612518d8b859d3e908daf18
syureu <tor01237@gmail.com>
2023-06-18 22:57:38 +0900

    Reacting to Input with State
    WIP

---
+++ 4. Managing State/1. Reacting to Input with State/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Form({ status = "empty" }) {
+  if (status === "success") {
+    return <h1>That's right!</h1>;
+  }
+  return (
+    <React.Fragment>
+      <h2>City quiz</h2>
+      <p>
+        In which city is there a billboard that turns air into drinkable water?
+      </p>
+      <form>
+        <textarea />
+        <br />
+        <button>Submit</button>
+      </form>
+    </React.Fragment>
+  );
+}
