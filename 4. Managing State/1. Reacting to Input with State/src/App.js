c4bd003ad68b58f7809009e51b12117f576b340f
syureu <tor01237@gmail.com>
2023-07-01 00:16:00 +0900

    Reacting to Input with State Challenge 1 of 3: Add and remove a CSS class
    solve

--- 4. Managing State/1. Reacting to Input with State/src/App.js
+++ 4. Managing State/1. Reacting to Input with State/src/App.js
@@ -1,10 +1,33 @@
 import React from "react";
 
+import { useState } from "react";
+
 export default function Picture() {
+  let [isHighlightPicture, setHighlightPicture] = useState(false);
+
+  function handleClick(e) {
+    e.stopPropagation();
+    if (e.target.tagName === "IMG") {
+      if (isHighlightPicture) {
+        return;
+      }
+    } else {
+      if (!isHighlightPicture) {
+        return;
+      }
+    }
+    setHighlightPicture(!isHighlightPicture);
+  }
+
   return (
-    <div className="background background--active">
+    <div
+      className={`background ${
+        !isHighlightPicture ? "background--active" : ""
+      }`}
+      onClick={handleClick}
+    >
       <img
-        className="picture"
+        className={`picture ${isHighlightPicture ? "picture--active" : ""}`}
         alt="Rainbow houses in Kampung Pelangi, Indonesia"
         src="https://i.imgur.com/5qwVYb1.jpeg"
       />
