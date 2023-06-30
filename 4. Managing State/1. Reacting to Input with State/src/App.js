89fe19cf2ca2af86cb8301feb386945d15ed7b91
syureu <tor01237@gmail.com>
2023-06-30 23:39:45 +0900

    Reacting to Input with State Challenge 1 of 3: Add and remove a CSS class
    problem

--- 4. Managing State/1. Reacting to Input with State/src/App.js
+++ 4. Managing State/1. Reacting to Input with State/src/App.js
@@ -1,64 +1,13 @@
 import React from "react";
 
-import { useState } from "react";
-
-export default function Form() {
-  const [answer, setAnswer] = useState("");
-  const [error, setError] = useState(null);
-  const [status, setStatus] = useState("typing");
-
-  if (status === "success") {
-    return <h1>That's right!</h1>;
-  }
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    setStatus("submitting");
-    try {
-      await submitForm(answer);
-      setStatus("success");
-    } catch (err) {
-      setStatus("typing");
-      setError(err);
-    }
-  }
-
-  function handleTextareaChange(e) {
-    setAnswer(e.target.value);
-  }
-
+export default function Picture() {
   return (
-    <React.Fragment>
-      <h2>City quiz</h2>
-      <p>
-        In which city is there a billboard that turns air into drinkable water?
-      </p>
-      <form onSubmit={handleSubmit}>
-        <textarea
-          value={answer}
-          onChange={handleTextareaChange}
-          disabled={status === "submitting"}
-        />
-        <br />
-        <button disabled={answer.length === 0 || status === "submitting"}>
-          Submit
-        </button>
-        {error !== null && <p className="Error">{error.message}</p>}
-      </form>
-    </React.Fragment>
+    <div className="background background--active">
+      <img
+        className="picture"
+        alt="Rainbow houses in Kampung Pelangi, Indonesia"
+        src="https://i.imgur.com/5qwVYb1.jpeg"
+      />
+    </div>
   );
-}
-
-function submitForm(answer) {
-  // Pretend it's hitting the network.
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      let shouldError = answer.toLowerCase() !== "lima";
-      if (shouldError) {
-        reject(new Error("Good guess but a wrong answer. Try again!"));
-      } else {
-        resolve();
-      }
-    }, 1500);
-  });
 }
