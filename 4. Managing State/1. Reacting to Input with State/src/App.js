c1d8d78a69030e6be8d27d2c1a72c0b6efc0654a
syureu <tor01237@gmail.com>
2023-07-03 23:50:45 +0900

    Reacting to Input with State Challenge 2 of 3: Profile editor
    problem

--- 4. Managing State/1. Reacting to Input with State/src/App.js
+++ 4. Managing State/1. Reacting to Input with State/src/App.js
@@ -1,36 +1,20 @@
 import React from "react";
 
-import { useState } from "react";
-
-export default function Picture() {
-  let [isHighlightPicture, setHighlightPicture] = useState(false);
-
-  function handleClick(e) {
-    e.stopPropagation();
-    if (e.target.tagName === "IMG") {
-      if (isHighlightPicture) {
-        return;
-      }
-    } else {
-      if (!isHighlightPicture) {
-        return;
-      }
-    }
-    setHighlightPicture(!isHighlightPicture);
-  }
-
+export default function EditProfile() {
   return (
-    <div
-      className={`background ${
-        !isHighlightPicture ? "background--active" : ""
-      }`}
-      onClick={handleClick}
-    >
-      <img
-        className={`picture ${isHighlightPicture ? "picture--active" : ""}`}
-        alt="Rainbow houses in Kampung Pelangi, Indonesia"
-        src="https://i.imgur.com/5qwVYb1.jpeg"
-      />
-    </div>
+    <form>
+      <label>
+        First name: <b>Jane</b>
+        <input />
+      </label>
+      <label>
+        Last name: <b>Jacobs</b>
+        <input />
+      </label>
+      <button type="submit">Edit Profile</button>
+      <p>
+        <i>Hello, Jane Jacobs!</i>
+      </p>
+    </form>
   );
 }
