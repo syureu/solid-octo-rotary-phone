6f928c8e2dede350e942fda75e6aaf4cb80daabf
syureu <tor01237@gmail.com>
2023-07-04 00:01:26 +0900

    Reacting to Input with State Challenge 2 of 3: Profile editor
    solve

--- 4. Managing State/1. Reacting to Input with State/src/App.js
+++ 4. Managing State/1. Reacting to Input with State/src/App.js
@@ -1,19 +1,52 @@
 import React from "react";
 
+import { useState } from "react";
+
 export default function EditProfile() {
+  let [firstName, setFirstName] = useState("Jane");
+  let [lastName, setLastName] = useState("Jacobs");
+  let [isInputMode, setInputMode] = useState(false);
+
+  function handleButtonClick(e) {
+    e.preventDefault();
+    setInputMode(!isInputMode);
+  }
+
+  function handleFirstNameInputOnchange(e) {
+    e.preventDefault();
+    setFirstName(e.target.value);
+  }
+
+  function handleLastNameInputOnchange(e) {
+    e.preventDefault();
+    setLastName(e.target.value);
+  }
+
   return (
     <form>
       <label>
-        First name: <b>Jane</b>
-        <input />
+        First name:{" "}
+        {isInputMode ? (
+          <input value={firstName} onChange={handleFirstNameInputOnchange} />
+        ) : (
+          <b>{firstName}</b>
+        )}
       </label>
       <label>
-        Last name: <b>Jacobs</b>
-        <input />
+        Last name:{" "}
+        {isInputMode ? (
+          <input value={lastName} onChange={handleLastNameInputOnchange} />
+        ) : (
+          <b>{lastName}</b>
+        )}
       </label>
-      <button type="submit">Edit Profile</button>
+      <button type="submit" onClick={handleButtonClick}>
+        Edit Profile
+      </button>
       <p>
-        <i>Hello, Jane Jacobs!</i>
+        <i>
+          Hello, {firstName} {lastName}!
+        </i>
       </p>
     </form>
   );
