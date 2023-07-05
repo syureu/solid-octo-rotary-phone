c3c5405335d66e0c4dcb64caa7f6fb30b8133265
syureu <tor01237@gmail.com>
2023-07-06 01:14:37 +0900

    Choosing the state structure
    WIP

--- 4. Managing State/2. Choosing the state structure/src/App.js
+++ 4. Managing State/2. Choosing the state structure/src/App.js
@@ -1,57 +1,35 @@
 import React from "react";
 
 import { useState } from "react";
+import { initialTravelPlan } from "./places.js";
 
-const initialItems = [
-  { title: "pretzels", id: 0 },
-  { title: "crispy seaweed", id: 1 },
-  { title: "granola bar", id: 2 },
-];
+function PlaceTree({ place }) {
+  const childPlaces = place.childPlaces;
+  return (
+    <li>
+      {place.title}
+      {childPlaces.length > 0 && (
+        <ol>
+          {childPlaces.map((place) => (
+            <PlaceTree key={place.id} place={place} />
+          ))}
+        </ol>
+      )}
+    </li>
+  );
+}
 
-export default function Menu() {
-  const [items, setItems] = useState(initialItems);
-  const [selectedId, setSelectedId] = useState(0);
-
-  const selectedItem = items.find((item) => item.id === selectedId);
-
-  function handleItemChange(id, e) {
-    setItems(
-      items.map((item) => {
-        if (item.id === id) {
-          return {
-            ...item,
-            title: e.target.value,
-          };
-        } else {
-          return item;
-        }
-      })
-    );
-  }
-
+export default function TravelPlan() {
+  const [plan, setPlan] = useState(initialTravelPlan);
+  const planets = plan.childPlaces;
   return (
     <React.Fragment>
-      <h2>What's your travel snack?</h2>
-      <ul>
-        {items.map((item, index) => (
-          <li key={item.id}>
-            <input
-              value={item.title}
-              onChange={(e) => {
-                handleItemChange(item.id, e);
-              }}
-            />{" "}
-            <button
-              onClick={() => {
-                setSelectedId(item.id);
-              }}
-            >
-              Choose
-            </button>
-          </li>
+      <h2>Places to visit</h2>
+      <ol>
+        {planets.map((place) => (
+          <PlaceTree key={place.id} place={place} />
         ))}
-      </ul>
-      <p>You picked {selectedItem.title}.</p>
+      </ol>
     </React.Fragment>
   );
 }
