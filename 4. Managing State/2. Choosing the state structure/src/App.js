38853c6466fb168a7a568c8f4e1b81136f8b2cc2
syureu <tor01237@gmail.com>
2023-07-06 00:43:41 +0900

    Choosing the state structure
    WIP

--- 4. Managing State/2. Choosing the state structure/src/App.js
+++ 4. Managing State/2. Choosing the state structure/src/App.js
@@ -2,37 +2,43 @@ import React from "react";
 
 import { useState } from "react";
 
-export default function MovingDot() {
-  const [position, setPosition] = useState({
-    x: 0,
-    y: 0,
-  });
+export default function FeedbackForm() {
+  const [text, setText] = useState("");
+  const [isSending, setIsSending] = useState(false);
+  const [isSent, setIsSent] = useState(false);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setIsSending(true);
+    await sendMessage(text);
+    setIsSending(false);
+    setIsSent(true);
+  }
+
+  if (isSent) {
+    return <h1>Thanks for feedback!</h1>;
+  }
+
   return (
-    <div
-      onPointerMove={(e) => {
-        setPosition({
-          x: e.clientX,
-          y: e.clientY,
-        });
-      }}
-      style={{
-        position: "relative",
-        width: "100vw",
-        height: "100vh",
-      }}
-    >
-      <div
-        style={{
-          position: "absolute",
-          backgroundColor: "red",
-          borderRadius: "50%",
-          transform: `translate(${position.x}px, ${position.y}px)`,
-          left: -10,
-          top: -10,
-          width: 20,
-          height: 20,
-        }}
+    <form onSubmit={handleSubmit}>
+      <p>How was your stay at The Prancing Pony?</p>
+      <textarea
+        disabled={isSending}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
       />
-    </div>
+      <br />
+      <button disabled={isSending} type="submit">
+        Send
+      </button>
+      {isSending && <p>Sending...</p>}
+    </form>
   );
+}
+
+// Pretend to send a message.
+function sendMessage(text) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, 2000);
+  });
 }
