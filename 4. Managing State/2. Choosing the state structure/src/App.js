0b56eac3e6ca7d3ce03654a3f342d88c22a019a7
syureu <tor01237@gmail.com>
2023-07-06 01:12:40 +0900

    Choosing the state structure
    WIP

--- 4. Managing State/2. Choosing the state structure/src/App.js
+++ 4. Managing State/2. Choosing the state structure/src/App.js
@@ -10,7 +10,9 @@ const initialItems = [
 
 export default function Menu() {
   const [items, setItems] = useState(initialItems);
-  const [selectedItem, setSelectedItem] = useState(items[0]);
+  const [selectedId, setSelectedId] = useState(0);
+
+  const selectedItem = items.find((item) => item.id === selectedId);
 
   function handleItemChange(id, e) {
     setItems(
@@ -41,7 +43,7 @@ export default function Menu() {
             />{" "}
             <button
               onClick={() => {
-                setSelectedItem(item);
+                setSelectedId(item.id);
               }}
             >
               Choose
