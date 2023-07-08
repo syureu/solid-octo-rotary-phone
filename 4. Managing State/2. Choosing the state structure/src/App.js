f99dabc92c2017c42f77ad91ef7711516f65999d
syureu <tor01237@gmail.com>
2023-07-08 21:51:40 +0900

    Choosing the state structure Challenge 3 of 4: Fix the disappearing selection
    problem

--- 4. Managing State/2. Choosing the state structure/src/App.js
+++ 4. Managing State/2. Choosing the state structure/src/App.js
@@ -1,70 +1,46 @@
 import React from "react";
 
 import { useState } from "react";
-import AddItem from "./AddItem.js";
-import PackingList from "./PackingList.js";
+import { initialLetters } from "./data.js";
+import Letter from "./Letter.js";
 
-let nextId = 3;
-const initialItems = [
-  { id: 0, title: "Warm socks", packed: true },
-  { id: 1, title: "Travel journal", packed: false },
-  { id: 2, title: "Watercolors", packed: false },
-];
+export default function MailClient() {
+  const [letters, setLetters] = useState(initialLetters);
+  const [highlightedLetter, setHighlightedLetter] = useState(null);
 
-export default function TravelPlan() {
-  const [items, setItems] = useState(initialItems);
-  const [total, setTotal] = useState(3);
-  const [packed, setPacked] = useState(1);
-
-  function handleAddItem(title) {
-    setTotal(total + 1);
-    setItems([
-      ...items,
-      {
-        id: nextId++,
-        title: title,
-        packed: false,
-      },
-    ]);
+  function handleHover(letter) {
+    setHighlightedLetter(letter);
   }
 
-  function handleChangeItem(nextItem) {
-    if (nextItem.packed) {
-      setPacked(packed + 1);
-    } else {
-      setPacked(packed - 1);
-    }
-    setItems(
-      items.map((item) => {
-        if (item.id === nextItem.id) {
-          return nextItem;
+  function handleStar(starred) {
+    setLetters(
+      letters.map((letter) => {
+        if (letter.id === starred.id) {
+          return {
+            ...letter,
+            isStarred: !letter.isStarred,
+          };
         } else {
-          return item;
+          return letter;
         }
       })
     );
   }
 
-  function handleDeleteItem(itemId) {
-    setTotal(total - 1);
-    if (items.filter((v) => v.id === itemId)[0].packed) {
-      setPacked(packed - 1);
-    }
-    setItems(items.filter((item) => item.id !== itemId));
-  }
-
   return (
     <React.Fragment>
-      <AddItem onAddItem={handleAddItem} />
-      <PackingList
-        items={items}
-        onChangeItem={handleChangeItem}
-        onDeleteItem={handleDeleteItem}
-      />
-      <hr />
-      <b>
-        {packed} out of {total} packed!
-      </b>
+      <h2>Inbox</h2>
+      <ul>
+        {letters.map((letter) => (
+          <Letter
+            key={letter.id}
+            letter={letter}
+            isHighlighted={letter === highlightedLetter}
+            onHover={handleHover}
+            onToggleStar={handleStar}
+          />
+        ))}
+      </ul>
     </React.Fragment>
   );
 }
